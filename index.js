a3169b8792576d31ed17605e211efa32cd32a365
sharjeel-coder <sharjeelkhalid231@gmail.com>
2026-08-06 13:30:45 +0500

    module1 completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 // this above is the built-in module.and that how the fs module is imported
 
 //
@@ -27,16 +29,47 @@ const http = require("http");
 // });
 
 //instead of doing the file read again and again we read at once and store when the program starts and save it to data
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8",
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
+// server
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("this is overview page");
-  } else if (pathName === "/product") {
-    res.end("this is the product page");
-  } else if (pathName === "/api") {
+  //es6 desturuting
+  const { query, pathname } = url.parse(req.url, true);
+
+  //overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "content-Type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  }
+
+  //products page
+  else if (pathname === "/product") {
+    res.writeHead(200, { "content-Type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  }
+  //api page
+  else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   } else {
